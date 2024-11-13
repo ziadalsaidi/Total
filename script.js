9e59b82e866cda75512aeaa665db138ac49ec415
ziadalsaidi <ziadalsaidi108@gmail.com>
2024-11-13 23:23:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function to show the payment popup
+function showPaymentPopup() {
+    document.getElementById('paymentPopup').style.display = 'flex';
+}
+
+// Function to close the payment popup
+function closePopup() {
+    document.getElementById('paymentPopup').style.display = 'none';
+}
+
